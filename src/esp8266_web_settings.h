f62791ca5530adef4b086c7057964bed6abad562
grmcdorman <17110555+grmcdorman@users.noreply.github.com>
2021-12-25 22:15:22 -0500

    Workflows (#3)
    * Add check-arduino (lint), compile examples
    
    * Put in example in compile-examples.
    
    * Fix errors found by lint/compile-examples
    
    * Remove 'official' from check-arduino, try new example path.
    
    * Put back 'examples' path, add required library deps.
    
    * Correct lint from library 'update' to 'submit', fix name.
    
    * Fix ASyncWebServer name, move SimpleWifiConfig to directory demanded by Arduino

---
+++ src/esp8266_web_settings.h
@@ -0,0 +1,6 @@
+// This file is required by Arduino libraries, despite the risk of this colliding with other header files.
+// The author's recommendation is that you ignore this file and include the file(s) of interest
+// such as <grmcdorman/WebServer.h>.
+
+// Include the main file. This does not include other classes.
+#include <grmcdorman/WebServer.h>
